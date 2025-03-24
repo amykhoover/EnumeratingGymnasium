a34eb31a47e6a80353e3c9de10d4d98abed1247b
amy <amyhoover@gmail.com>
2025-03-23 23:34:10 -0400

    Added general folder.

--- enumerating_gymnasium_envs.py
+++ enumerating_gymnasium_envs.py
@@ -3,23 +3,27 @@ from PIL import Image
 import datetime
 import ale_py
 import os 
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 
 if __name__ == '__main__':
-    if not os.path.exists('envs_info'):
+    if not os.path.exists('envs_info/'):
         os.makedirs('envs_info')
+    if not os.path.exists('envs_info/general'):
+        os.makedirs('envs_info/general')
     if not os.path.exists('envs_info/phys2d'):
         os.makedirs('envs_info/phys2d')
     if not os.path.exists('envs_info/tabular'):
         os.makedirs('envs_info/tabular')
     if not os.path.exists('envs_info/ALE'):
         os.makedirs('envs_info/ALE')
-    #env_name = "CartPole-v1"
+
+
     gym.register_envs(ale_py)
     current_gym_envs = gym.envs.registration.registry.keys()
     
-    for envname in current_gym_envs:
-        print(envname)
-    print(f"-----------------------")
+
     for env_name in current_gym_envs:
         if 'Joust' not in env_name and 'Warlords' not in env_name and 'MazeCraze' not in env_name and 'Combat' not in env_name and 'Reacher' not in env_name and 'Pusher' not in env_name and 'InvertedPendulum' not in env_name and 'InvertedDoublePendulum' not in env_name and 'Ant' not in env_name and 'HalfCheetah' not in env_name and 'Hopper' not in env_name and 'Walker2d' not in env_name and 'Swimmer' not in env_name and 'Humanoid' not in env_name and 'GymV21Environment' not in env_name and 'GymV26Environment' not in env_name:
             print(f"Environment Name: {env_name}")
@@ -28,10 +32,21 @@ if __name__ == '__main__':
             datestr = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
             #print(datestr)
             img = Image.fromarray(env.render())
+
+            if '/' not in env_name:
+                env_name = f"general/{env_name}"
             img.save(f"envs_info/{env_name}_{datestr}.png")
 
             with open(f"envs_info/{env_name}_{datestr}.txt", 'w') as f:
                 print(env_name, file=f)
-                print("env.reset() Obervation")
-                print(obs, file=f)
+                print("\nMetadata", file=f)
+                print(env.metadata, file=f)
+                print("\n\nAction Space", file=f)
+                print(env.action_space, file=f)
+                print("\nObservation Space", file=f)
+                print(env.observation_space, file=f)
+                print("\nEnvironment Dictionary", file=f)
                 print(env.__dict__, file=f)
+
+                print("\n\nInitial Obervation", file=f)
+                print(obs, file=f)
